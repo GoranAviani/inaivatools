5840645fd387400df571dafc5e90e516691ae393
goranaviani <goranaviani@gmail.com>
2019-01-25 22:28:47 +0100

    urls done and smaller html stuff

--- expandeduser/forms.py
+++ expandeduser/forms.py
@@ -4,16 +4,16 @@ from .models import custom_user
 
 
 
-#class UserChangeForm A form used in the admin interface to change a user’s information and permissions.
+#class UserCreationForm A ModelForm for creating a new user.
+#It has three fields: username (from the user model), password1, and password2. It verifies that password1 and password2 match, validates the password using validate_password(), and sets the user’s password using set_password().
 class custom_user_creation_form(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = custom_user
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'mobileNumber')
 
 
-#class UserCreationForm A ModelForm for creating a new user.
-#It has three fields: username (from the user model), password1, and password2. It verifies that password1 and password2 match, validates the password using validate_password(), and sets the user’s password using set_password().
+#class UserChangeForm A form used in the admin interface to change a user’s information and permissions.
 class custom_user_change_form(UserChangeForm):
 
     class Meta:
--- expandeduser/views.py
+++ expandeduser/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views import generic
+
+from .forms import custom_user_creation_form
+
+class sign_up(generic.CreateView):
+    form_class = custom_user_creation_form
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
