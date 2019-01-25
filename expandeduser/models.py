282c6abf73ec162442dd93db13129d438a1ce28a
goranaviani <goranaviani@gmail.com>
2019-01-25 21:02:17 +0100

    setup of users and CustoUser Class in a model

---
+++ expandeduser/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    mobileNumber = models.CharField(max_length=50,null=True, blank=True)
+    location = models.CharField(max_length=30,null=True, blank=True)
+    birthDate = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.email
--- inaivatools/settings.py
+++ inaivatools/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'expandeduser.apps.ExpandeduserConfig',
 ]
+
+AUTH_USER_MODEL = 'expandeduser.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
