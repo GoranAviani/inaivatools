75a5109f35f6fa08caf8fdac119ea522f5b3986c
goranaviani <goranaviani@gmail.com>
2019-01-26 21:31:33 +0100

    merge text layout

--- mergetextlines/forms.py
+++ mergetextlines/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class merge_text_input_form(forms.Form):
-    link_name = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}))
-    link_url = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}))
+    inputText = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'merge-text-input'}))
+    joinBy = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}), initial=',')
    
