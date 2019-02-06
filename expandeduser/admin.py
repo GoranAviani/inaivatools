4bc0f2b204e54a96cd1fbb7f6260b98b97d9376f
goranaviani <goranaviani@gmail.com>
2019-02-06 17:24:35 +0100

    layout of users admin page

--- expandeduser/admin.py
+++ expandeduser/admin.py
@@ -13,7 +13,7 @@ class custom_user_admin(UserAdmin):
     add_form = custom_user_creation_form
     form = custom_user_change_form
     model = custom_user
-   # list_display = ['email', 'username','mobileNumber']
+    list_display = ['username','email','mobileNumber','first_name', 'last_name','is_active','is_staff','is_superuser','last_login' ]
     fieldsets = (
     (None, {'fields': ('username', 'email', 'password')}),
    ('Basic info', {'fields': ( 'first_name', 'last_name','birthDate')}),
--- formattelnumbers/admin.py
+++ formattelnumbers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+# users/admin.py
+from .models import uploaded_documents
