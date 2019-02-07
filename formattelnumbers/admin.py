235571901711d1410b137c4a5814228b5d9ffb3b
goranaviani <goranaviani@gmail.com>
2019-02-07 16:57:25 +0100

    uploaded_documents_admin

--- formattelnumbers/admin.py
+++ formattelnumbers/admin.py
@@ -7,19 +7,9 @@ from django.contrib import admin
 # users/admin.py
 from .models import uploaded_documents
 
+class uploaded_documents_admin(admin.ModelAdmin):
+  list_display = ['description','document','uploaded_at']
+  list_filter = ('uploaded_at','description')
+  date_hierarchy = 'uploaded_at'
 
-#class uploaded_documents_admin(uploaded_documents):
-    
-   # add_form = custom_user_creation_form
-  #  form = custom_user_change_form
-#    model = uploaded_documents
-   # list_display = ['username','email','mobileNumber','first_name', 'last_name','is_active','is_staff','is_superuser','last_login' ]
-  #  fieldsets = (
-  #  (None, {'fields': ('username', 'email', 'password')}),
- #  ('Basic info', {'fields': ( 'first_name', 'last_name','birthDate')}),
- #   ('Other info', {'fields': ( 'mobileNumber', 'location')}),
- # ('Permissions', {'fields': ('is_superuser', 'is_staff', 'is_active','groups')}),
-  #      ('Important dates', {'fields': ( 'last_login', 'date_joined')}),
-  # )
-
-admin.site.register(uploaded_documents)
+admin.site.register(uploaded_documents, uploaded_documents_admin)
