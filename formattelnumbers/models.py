b402f5af39ab9d71347b78d41db25bfeb004e9ba
goranaviani <goranaviani@gmail.com>
2019-02-12 21:02:41 +0100

    overwriting a file in the app if file with the same name is to be uploaded

--- formattelnumbers/models.py
+++ formattelnumbers/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.core.files.storage import FileSystemStorage
+
+
+class OverwriteStorage(FileSystemStorage):
+    '''
+    Muda o comportamento padrão do Django e o faz sobrescrever arquivos de
+    mesmo nome que foram carregados pelo usuário ao invés de renomeá-los.
+    '''
+    def get_available_name(self, name, max_length=None):
+        if self.exists(name):
+            #os.remove(os.path.join(settings.MEDIA_ROOT, name))
+            self.delete(name)
+        return name
 
 
 class uploaded_documents(models.Model):
     description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='format_tel_number/')
+    document = models.FileField(storage=OverwriteStorage(), upload_to='format_tel_number/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
