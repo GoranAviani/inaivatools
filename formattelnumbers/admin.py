a5ea283466b6454910d56b424cc731f9489f5535
goranaviani <goranaviani@gmail.com>
2019-02-15 13:59:59 +0100

    added document user to admin dashboard

--- formattelnumbers/admin.py
+++ formattelnumbers/admin.py
@@ -8,7 +8,7 @@ from django.contrib import admin
 from .models import uploaded_documents
 
 class uploaded_documents_admin(admin.ModelAdmin):
-  list_display = ['description','document','uploaded_at']
+  list_display = ['document','document_user','uploaded_at','description']
   list_filter = ('uploaded_at','description')
   date_hierarchy = 'uploaded_at'
 
