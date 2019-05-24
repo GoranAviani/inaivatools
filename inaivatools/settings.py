13f0d7b4406abbc2182b111e40906a3d9e81d681
goranaviani <goranaviani@gmail.com>
2019-05-24 17:05:49 +0200

    try

--- inaivatools/settings.py
+++ inaivatools/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
