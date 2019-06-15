f19f93495ff6eabff7f6fefb2771686337fe22b8
goranaviani <goranaviani@gmail.com>
2019-06-15 18:10:13 +0200

    result fields not requirerd

--- comparelists/forms.py
+++ comparelists/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class compare_lists_input_form(forms.Form):
     inputText1 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'compare-lists-input'}))
     inputText2 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'compare-lists-input'}))
-    resultFoundInList2 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'compare-lists-input'}))
-    resultMissingInList2 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'compare-lists-input'}))
+    resultFoundInList2 = forms.CharField(label='',required=False, widget=forms.Textarea(attrs={'class':'compare-lists-input'}))
+    resultMissingInList2 = forms.CharField(label='',required=False, widget=forms.Textarea(attrs={'class':'compare-lists-input'}))
 
     
