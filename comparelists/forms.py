280896a533333d244719a5be40b9988416bbe1fa
goranaviani <goranaviani@gmail.com>
2019-06-15 16:51:32 +0200

    compare lists forntend

--- comparelists/forms.py
+++ comparelists/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class compare_lists_input_form(forms.Form):
-    inputText1 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'merge-text-input'}))
-    inputText2 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'merge-text-input'}))
-    resultFoundInList2 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'merge-text-input'}))
-    resultMissingInList2 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'merge-text-input'}))
+    inputText1 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'compare-lists-input'}))
+    inputText2 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'compare-lists-input'}))
+    resultFoundInList2 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'compare-lists-input'}))
+    resultMissingInList2 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'compare-lists-input'}))
 
     
