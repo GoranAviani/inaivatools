97f86aea79010c7fcf94970bee02e127a41f41dd
goranaviani <goranaviani@gmail.com>
2019-06-15 15:36:25 +0200

    Compare lists takes input in rows and returns lists to customer

--- comparelists/forms.py
+++ comparelists/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 class compare_lists_input_form(forms.Form):
-    inputList1 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'merge-text-input'}))
-    inputList2 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'merge-text-input'}))
+    inputText1 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'merge-text-input'}))
+    inputText2 = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'merge-text-input'}))
+    listDifferences = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'merge-text-input'}))
+
     
--- comparelists/views.py
+++ comparelists/views.py
@@ -5,24 +5,32 @@ from .forms import compare_lists_input_form
 # Create your views here.
 def compare_lists(request):
     
-    '''
+    
     if request.method == 'POST':
-        textToJoin = merge_text_input_form(request.POST)
-        if textToJoin.is_valid():
+        listsToCompare = compare_lists_input_form(request.POST)
+        if listsToCompare.is_valid():
 
-            textdata = textToJoin['inputText'].value()
-            mergeBy = textToJoin['joinBy'].value()
+            inputList1 = listsToCompare['inputText1'].value()
+            inputList2 = listsToCompare['inputText2'].value()
+            listDifferences = ""
+
+##            mergeBy = listsToCompare['joinBy'].value()
             
-            #replacing \r\n with a inputed character
-            textdata = textdata.replace("\r\n", mergeBy)
-            data = {'inputText': textdata, 'joinBy': mergeBy}
-            textToJoin = merge_text_input_form(initial=data)
+            #replacing \r\n with a "," character and then create lists from inputLists strings
+            inputList1 = inputList1.replace("\r\n", ",")
+            inputList2 = inputList2.replace("\r\n", ",")
+            inputList1 = list(inputList1.split(","))
+            inputList2 = list(inputList2.split(","))
             
-            return render(request, 'mergeTextLines/mergelines.html', {'textToJoin': textToJoin})
+            #Process the lists and get the result
+             
+            
+            #Display the result data
+            data = {'inputText1': inputList1, 'inputText2': inputList2, 'listDifferences': listDifferences}
+            listsToCompare = compare_lists_input_form(initial=data)
+            
+            return render(request, 'compareLists/comparelists.html', {'listsToCompare': listsToCompare})
 
     else:
-        textToJoin = merge_text_input_form()
-        return render(request, 'mergeTextLines/mergelines.html', {'textToJoin': textToJoin})
-    '''
-    listsToCompare = compare_lists_input_form()
-    return render(request, 'compareLists/comparelists.html', {'listsToCompare': listsToCompare})
+        listsToCompare = compare_lists_input_form()
+        return render(request, 'compareLists/comparelists.html', {'listsToCompare': listsToCompare})
