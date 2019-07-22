831b1439d5a815e16cedb44c2fd18a9ef032d8bf
goranaviani <goranaviani@gmail.com>
2019-07-22 19:24:00 +0200

    try except

--- apirelay/views.py
+++ apirelay/views.py
@@ -43,45 +43,61 @@ class format_tel_numbers_api(APIView):
 
 
 class format_tel_numbers_api(APIView):
-    serializer_class = TelNumberSerializer
+   serializer_class = TelNumberSerializer
 
 
-    def get(self, request):      
-        
-        listOfTelNumbers=[]
-        telCountry = {}
-        telNumberList = {}
+   def get(self, request):     
+      
+       listOfTelNumbers=[]
+       telCountry = {}
+       telNumberList = {}
 
-        dict= request._request.GET #geting the data from QueryDict
-        dict = dict.dict() #QueryDict to Python dict
-        
-        if bool(dict) is not False: # is dict is not empty then look for these fields
-            if 'telCountry' in dict.keys():
-                telCountry = dict["telCountry"]
-                telNumberList = dict["telNumberList"]
-                listOfTelNumbers = telNumberList.split(",")
-                
-                result = dataProcessing.check_country_code(telCountry)
-                if result == "non_existing_country":
-                    data = {'responseMessage': "Failure. Unknown country", "telCountry": "Unknown country code", 'telNumberList': "This field may not be blank."}
-                    serializer = TelNumberSerializer(data=data)
-                    if serializer.is_valid():
-                        return Response(serializer.data, status=status.HTTP_200_OK)
-                    
+       dict= request._request.GET #geting the data from QueryDict
+       dict = dict.dict() #QueryDict to Python dict
+      
+      # if bool(dict) is not False: # is dict is not empty then look for these fields
 
-        #Caling a view from another app
-        listResult = process_numbers(listOfTelNumbers, telCountry)
-        #print(listResult)
 
-        listResultChar = ",".join(listResult)
-        #print(listResultChar)
+       try:
+           telCountry = dict["country_code"]
+       except:
+           try:
+               telNumberList = dict["telephone_numbers"]
+           except:
+               return Response({'status': "FAILED ", "message": "No country code and no telephone numbers"})
 
-        data = {'responseMessage': "Success", "telCountry": telCountry, 'telNumberList': listResultChar}
-        serializer = TelNumberSerializer(data=data)
-        if serializer.is_valid():
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+           return Response({'status': "FAILED ", "message": "No country code"})
+
+
+       try:
+           telNumberList = dict["telephone_numbers"]
+       except:
+           return Response({'status': "FAILED ", "message": "No telephone numbers"})
+
+
+
+     
+       telCountry = dict["country_code"]
+       result = dataProcessing.check_country_code(telCountry)
+       if result == "non_existing_country":
+           data = {'status': "FAILED", "message": "Unknown country code"}
+           return Response(data, status=status.HTTP_200_OK)
+          
+       telNumberList = dict["telephone_numbers"]
+       listOfTelNumbers = telNumberList.split(",")
+      
+
+
+
+       #Caling a view from another app
+       listResult = process_numbers(listOfTelNumbers, telCountry)
+       #print(listResult)
+
+       listResultChar = ",".join(listResult)
+       #print(listResultChar)
+
+       data = {'status': "SUCCESS", "country_code": telCountry, 'telephone_numbers': listResultChar}
+       return Response(data, status=status.HTTP_200_OK)
 
 
 
