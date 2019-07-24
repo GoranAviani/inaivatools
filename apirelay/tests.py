9e116589f1c294eb22b464a890ae30c0b91f4fe2
goranaviani <goranaviani@gmail.com>
2019-07-24 15:23:26 +0200

    first django tests

--- apirelay/tests.py
+++ apirelay/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+# calling a test: ./manage.py test apirelay.tests
+
+from apirelay.dataProcessing import check_country_code
+
+class APIrelayTestCase(TestCase):
+
+    def test_SE_country_code(self):
+        result = check_country_code("SE")
+        self.assertEqual(result, "existing_country")
+
+    def test_YU_country_code(self):
+        result = check_country_code("YU")
+        self.assertEqual(result, "non_existing_country")
