8f678a30cace000da30ed9a5c79a84490f5d52dc
goranaviani <goranaviani@gmail.com>
2019-09-29 19:35:38 +0200

    forms for split lines app

--- splittextbychar/forms.py
+++ splittextbychar/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class split_text_input_form(forms.Form):
     inputText = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'merge-text-input'}))
-    splitBy = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}), initial=',')
+    splitBy = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}), initial="','")
     
