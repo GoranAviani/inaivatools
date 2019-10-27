25c42c3d235f55487d50ef0d97ccfb328b7fb930
goranaviani <goranaviani@gmail.com>
2019-10-27 15:29:44 +0100

    testing settings

---
+++ inaivatools/local_settings.py
@@ -0,0 +1,6 @@
+#This is the local setting that are not intended to be seen
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'ld-5bmh8$0evt*r5$$*_u@atk6(zx+)m)%gm%m1hp45o$gjtv3'
+
+ALLOWED_HOSTS = ['www.tools.incodaq.com', 'tools.incodaq.com', '157.230.127.167', '127.0.0.1', 'localhost']
