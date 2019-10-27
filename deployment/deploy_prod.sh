99fd410f0450df98b2377d3c54d45d0d284c8b0c
goranaviani <goranaviani@gmail.com>
2019-10-27 15:34:35 +0100

    deploy_prod scrit

---
+++ deployment/deploy_prod.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+ssh root@2008anja:157.230.127.167 <<EOF
+cd inaivatoolsenv
+source inaivatoolsenv/bin/activate
+cd ..
+cd inaivatools
+git pull
+pip install -r requirements.txt
+./manage.py migrate
+systemctl restart gunicorn
+systemctl daemon-reload
+systemctl restart gunicorn.socket gunicorn.service
+nginx -t && sudo systemctl restart nginx
+exit
+EOF
