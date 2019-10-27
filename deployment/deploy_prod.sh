798276674e4eb64bcf8366e01038240a2316f4ed
goranaviani <goranaviani@gmail.com>
2019-10-27 15:52:23 +0100

    try

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-ssh root@2008anja:157.230.127.167 <<EOF
-cd inaivatoolsenv
-source inaivatoolsenv/bin/activate
-cd ..
-cd inaivatools
-git pull
-pip install -r requirements.txt
-./manage.py migrate
-systemctl restart gunicorn
-systemctl daemon-reload
-systemctl restart gunicorn.socket gunicorn.service
-nginx -t && sudo systemctl restart nginx
-exit
+ssh root@157.230.127.167 <<EOF
+    2008anja
+    cd inaivatoolsenv
+    source inaivatoolsenv/bin/activate
+    cd ..
+    cd inaivatools
+    git pull
+    pip install -r requirements.txt
+    ./manage.py migrate
+    systemctl restart gunicorn
+    systemctl daemon-reload
+    systemctl restart gunicorn.socket gunicorn.service
+    nginx -t && sudo systemctl restart nginx
+    exit
 EOF
