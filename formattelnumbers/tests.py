f3a367784fa8f07a8bad3eb224dca700a979a72e
goranaviani <goranaviani@gmail.com>
2019-11-04 20:17:40 +0100

    tests for remove_country_code fun

--- formattelnumbers/tests.py
+++ formattelnumbers/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 
-from .cleaningTelNum import remove_spaces_from_tel, remove_plus_from_tel
+from .cleaningTelNum import remove_spaces_from_tel, remove_plus_from_tel, remove_country_code
 
 
 class RemoveSpacesFromTel(TestCase):
@@ -24,3 +24,17 @@ class RemovePlusFromTel(TestCase):
     def test_remove_first_plus(self):
         result = remove_plus_from_tel("+073123123")
         self.assertEqual(result, ("073123123"))
+
+class RemoveCountryCode(TestCase):
+
+    def test_remove_country_code_se_46(self):
+        result = remove_country_code("4673123123", "SE")
+        self.assertEqual(result, ("73123123"))
+
+    def test_remove_country_code_se_046(self):
+        result = remove_country_code("04673123123", "SE")
+        self.assertEqual(result, ("73123123"))
+
+    def test_remove_country_code_se_0046(self):
+        result = remove_country_code("004673123123", "SE")
+        self.assertEqual(result, ("73123123"))
