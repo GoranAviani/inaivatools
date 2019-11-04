cb135194a1778b1d62e2242819f3cfc49ac67af8
goranaviani <goranaviani@gmail.com>
2019-11-04 19:57:37 +0100

    Tests for remove_first_space_from_tel fun

--- formattelnumbers/tests.py
+++ formattelnumbers/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .cleaningTelNum import remove_first_space_from_tel
+
+
+class CleaningTelNumber(TestCase):
+    
+    def test_remove_first_space_from_tel(self):
+        result = remove_first_space_from_tel(" 073123123")
+        self.assertEqual(result, ("073123123"))
+
+    def test_remove_first_space_from_tel(self):
+        result = remove_first_space_from_tel("  073123123")
+        self.assertEqual(result, ("073123123"))
+
+    def test_remove_first_space_from_tel(self):
+        result = remove_first_space_from_tel("  073 123 123")
+        self.assertEqual(result, ("073123123"))
