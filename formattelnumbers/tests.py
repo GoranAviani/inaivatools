5c0aa8435838aa9446590132e166721b2fc400f4
goranaviani <goranaviani@gmail.com>
2019-11-04 20:08:10 +0100

    new test for removing first plus from tel function

--- formattelnumbers/tests.py
+++ formattelnumbers/tests.py
@@ -2,10 +2,10 @@ from django.test import TestCase
 
 # Create your tests here.
 
-from .cleaningTelNum import remove_spaces_from_tel
+from .cleaningTelNum import remove_spaces_from_tel, remove_plus_from_tel
 
 
-class CleaningTelNumber(TestCase):
+class RemoveSpacesFromTel(TestCase):
     
     def test_remove_spaces_from_tel_one_space_at_start(self):
         result = remove_spaces_from_tel(" 073123123")
@@ -17,4 +17,10 @@ class CleaningTelNumber(TestCase):
 
     def test_remove_spaces_from_tel_more_spaces(self):
         result = remove_spaces_from_tel("  073 123 123")
+        self.assertEqual(result, ("073123123"))
+
+class RemovePlusFromTel(TestCase):
+
+    def test_remove_first_plus(self):
+        result = remove_plus_from_tel("+073123123")
         self.assertEqual(result, ("073123123"))
