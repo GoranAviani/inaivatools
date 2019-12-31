9bb707cb0b9007da4ed008b74670c35b7f3fde26
goranaviani <goranaviani@gmail.com>
2019-12-31 11:24:47 +0100

    Testing Jenkins deployment with raspberry pi

--- formattelnumbers/tests.py
+++ formattelnumbers/tests.py
@@ -13,7 +13,7 @@ class RemoveSpacesFromTel(TestCase):
 
     def test_remove_spaces_from_tel_two_spaces_at_start(self):
         result = remove_spaces_from_tel("  073123123")
-        self.assertEqual(result, ("073123123a"))
+        self.assertEqual(result, ("073123123"))
 
     def test_remove_spaces_from_tel_more_spaces(self):
         result = remove_spaces_from_tel("  073 123 123")
