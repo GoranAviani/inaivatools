e462c355bf0e345f836682a95cedf5ff836f18e3
goranaviani <goranaviani@gmail.com>
2020-01-19 14:47:25 +0100

    catching text that does not belong to international morse code alphabed and dislaying message to the user

--- morsecode/views.py
+++ morsecode/views.py
@@ -11,6 +11,38 @@ def code_decode(**kwargs):
         codeOrDecode = kwargs["codeOrDecode"]
     except:
         return "error", "Can not fetch morse code data from the users input"
+    # Dictionary representing the morse code chart
+    MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                       'C': '-.-.', 'D': '-..', 'E': '.',
+                       'F': '..-.', 'G': '--.', 'H': '....',
+                       'I': '..', 'J': '.---', 'K': '-.-',
+                       'L': '.-..', 'M': '--', 'N': '-.',
+                       'O': '---', 'P': '.--.', 'Q': '--.-',
+                       'R': '.-.', 'S': '...', 'T': '-',
+                       'U': '..-', 'V': '...-', 'W': '.--',
+                       'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                       '1': '.----', '2': '..---', '3': '...--',
+                       '4': '....-', '5': '.....', '6': '-....',
+                       '7': '--...', '8': '---..', '9': '----.',
+                       '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                       '?': '..--..', '/': '-..-.', '-': '-....-',
+                       '(': '-.--.', ')': '-.--.-'}
+
+    #check if all letters are in the morse alphabet. if not return a message to user
+    textToProcess = list(inputText)
+    for x in textToProcess:
+        if x.upper() not in MORSE_CODE_DICT:
+            return "error", "Text or parts of text do not belong to international morse code alphabet"
+
+    resultText = ""
+    if codeOrDecode == "code":
+        textToProcess = list(inputText)
+        for x in textToProcess:
+            pass
+    elif codeOrDecode == "decode":
+        pass
+
+
 
     return "success", "result text"
 
